767d045639bcc6faa139d94a24c489b862448835
Sahayak <sahayaksharma6@gmail.com>
2025-10-25 17:12:56 +0530

    Initial Commit

---
+++ ExpenseTrackerSwiftProject/ExpenseTrackerSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerSwiftProjectApp.swift
+//  ExpenseTrackerSwiftProject
+//
+//  Created by Sahayak Sharma on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
